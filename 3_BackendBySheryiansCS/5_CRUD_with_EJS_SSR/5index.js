745491094bf8c66f1881f0bb34fba6a46cde1bd7
ASHFAQ-91 <ashfaqqureshi242@gmail.com>
2025-10-09 04:05:53 +0530

    Update 3_Back.../5_CRUD_with_EJS_SSR folder

--- 3_BackendBySheryiansCS/5_CRUD_with_EJS_SSR/5index.js
+++ 3_BackendBySheryiansCS/5_CRUD_with_EJS_SSR/5index.js
@@ -1,6 +1,7 @@
 import express from 'express';  // importing express
 import path from 'path';
 import { fileURLToPath } from 'url';  // NEW: Required for ES Modules
+import userModel from './models/user.js';
 
 // FIX: Define __dirname for ES Module Scope
 // ----------------------------------------------------
@@ -19,9 +20,31 @@ const staticPath = path.join(__dirname, './public');
 app.use(express.static(staticPath));  // working with file and directory paths.
 
 
-
+// Route
 app.get("/", (req, res) => {
     res.render('script.ejs');
+})
+
+app.get("/read", async (req, res) => {
+    let allusers = await userModel.find();
+    res.render('read.ejs', { allusers });
+})
+
+app.get("/delete/:id", async (req, res) => {
+    await userModel.findOneAndDelete({ _id: req.params.id });
+    res.redirect("/read");
+})
+
+app.post("/create", async (req, res) => {
+    let { his_name, his_email, his_image_url } = req.body;
+
+    await userModel.create({
+        name: his_name,
+        email: his_email,
+        image: his_image_url
+    });
+    // Redirect to the /read route to show the updated list
+    res.redirect("/read");
 })
 
 app.listen(3000, () => {
---
+++ 3_BackendBySheryiansCS/5_CRUD_with_EJS_SSR/models/user.js
@@ -0,0 +1,24 @@
+import mongoose from 'mongoose';
+
+mongoose.connect("mongodb://127.0.0.1:27017/testapp1");
+
+const userSchema = mongoose.Schema({
+    name: String,
+    email: String,
+    image: String
+})
+
+
+// model allow you to perform operations like CREATE, READ, UPDATE, DELETE.
+// model always create in plural form. like u write "user" then it will "users".
+// "exports" is a property, not a method. 'users' is a collection name.
+//$ Way 1 for creating & exporting module. ES Module Syntax
+const user = mongoose.model('user', userSchema);
+export default user;
+
+//$ Way 2 for creating & exporting module. CommonJS Syntax
+// module.exports = mongoose.model("user", userSchema);
+
+
+// Extra information:
+// mongoose.Schema is a constructor function
